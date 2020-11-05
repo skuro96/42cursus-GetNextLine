e70908c49780fbf055340300e4bb87aa81e7ed60
Sho Kurosu <xsho0212@outlook.com>
2020-11-06 01:16:56 +0900

    できたんごおおおお

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skurosu <skurosu@student.42tokyo.j>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/06 00:20:53 by skurosu           #+#    #+#             */
+/*   Updated: 2020/11/06 00:24:46 by skurosu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 int		free_return(char **s1, char **s2, int ret)
@@ -33,20 +45,20 @@ int		save_buf(char **save, char **buf)
 
 int		cut_endl(char **save, char **line)
 {
-	int		endl_idx;
+	int		endl;
 	char	*tmp;
 
-	endl_idx = ft_strchr(*save, '\n');
-	if (endl_idx < 0)
+	endl = ft_strchr(*save, '\n');
+	if (endl < 0)
 	{
 		if (!(*line = ft_substr(*save, ft_strlen(*save))))
-			 (free_return(save, NULL, READ_ERROR));
+			(free_return(save, NULL, READ_ERROR));
 		return (free_return(save, NULL, READ_EOF));
 	}
-	if (!(*line = ft_substr(*save, endl_idx)))
+	if (!(*line = ft_substr(*save, endl)))
 		return (free_return(save, NULL, READ_ERROR));
 	tmp = *save;
-	if (!(*save = ft_substr(*save + endl_idx + 1, ft_strlen(*save + endl_idx + 1))))
+	if (!(*save = ft_substr(*save + endl + 1, ft_strlen(*save + endl + 1))))
 		return (free_return(&tmp, line, READ_ERROR));
 	return (free_return(&tmp, NULL, READ));
 }
@@ -59,6 +71,8 @@ int		get_next_line(int fd, char **line)
 
 	if (!line || fd < 0 || BUFFER_SIZE <= 0)
 		return (READ_ERROR);
+	if (ft_strchr(save, '\n') != -1)
+		return (cut_endl(&save, line));
 	if (!(buf = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
 		return (free_return(&save, NULL, READ_ERROR));
 	while ((ret = read(fd, buf, BUFFER_SIZE)) > 0)
--- get_next_line.h
+++ get_next_line.h
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skurosu <skurosu@student.42tokyo.j>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/06 00:21:06 by skurosu           #+#    #+#             */
+/*   Updated: 2020/11/06 00:22:33 by skurosu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
-#define READ 1
-#define READ_EOF 0
-#define READ_ERROR -1
+# define READ 1
+# define READ_EOF 0
+# define READ_ERROR -1
 
 size_t	ft_strlen(char *str);
 char	*ft_substr(char const *str, size_t len);
